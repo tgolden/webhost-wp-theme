0a530121d81427e10a77e6ad46275a2078212822
tgolden <contact@impressioncomputers.com>
2009-03-15 16:39:25 -0400

    Starting from scratch

--- thematic/sidebar-index-top.php
+++
@@ -1,5 +0,0 @@
-<?php if ( is_sidebar_active('index-top') ) { // there is active widgets for this sidebar
-    echo '<div id="index-top" class="aside">'. "\n" . '<ul class="xoxo">' . "\n";
-    dynamic_sidebar('index-top');
-    echo '</ul>' . "\n" . '</div><!-- #index-top .aside -->'. "\n";
-} ?>
